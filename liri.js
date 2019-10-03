91dbbf7cc4b3d64cc040f5af74bea98a72973ee4
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-10-03 10:18:48 +1000

    concert this is loading

--- liri.js
+++ liri.js
@@ -1,5 +1,35 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var keys = require("./key.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var userInput = process.argv[3];
+
+var axios = require("axios");
+
+switch (command){
+    case "concert-this":
+
+axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+    function(response){
+        console.log("-------------- GIG GUIDE: " + userInput + " --------------")
+        console.log("\r\nGig One:")
+        console.log(response.data[0].venue.name)
+        console.log(response.data[0].venue.city + ", " + response.data[0].venue.country)
+        console.log(response.data[0].datetime)
+        console.log("\r\nGig Two:")
+        console.log(response.data[1].venue.name)
+        console.log(response.data[1].venue.city + ", " + response.data[1].venue.country)
+        console.log(response.data[1].datetime)
+        console.log("\r\nGig Three:")
+        console.log(response.data[2].venue.name)
+        console.log(response.data[2].venue.city + ", " + response.data[2].venue.country)
+        console.log(response.data[2].datetime)
+    }
+);
+    
+}
+
+
