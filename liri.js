277e16be562ec9d997bda099833eea54458316ba
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-10-03 11:20:24 +1000

    movie guide is working

--- liri.js
+++ liri.js
@@ -31,23 +31,40 @@ switch (command){
   break;
   
   case "spotify-this-song":
-
             
-spotify.search({ type: 'track', query: userInput }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-    console.log("-------------- SONG GUIDE: " + userInput + " --------------\r\n")
+        spotify.search({ type: 'track', query: userInput }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
+            }
+            console.log("-------------- SONG GUIDE: " + userInput + " --------------\r\n")
 
-    for (var i = 0; i <  data.tracks.items.length; i++){
-        console.log(data.tracks.items[i].artists[0].name); 
-        console.log(data.tracks.items[i].name); 
-        console.log(data.tracks.items[i].external_urls.spotify); 
-        console.log(data.tracks.items[i].album.name); 
-        console.log("\r\n------------------------------------\r\n")
-    }
-    
-    });
+            for (var i = 0; i <  data.tracks.items.length; i++){
+                console.log(data.tracks.items[i].artists[0].name); 
+                console.log(data.tracks.items[i].name); 
+                console.log(data.tracks.items[i].external_urls.spotify); 
+                console.log(data.tracks.items[i].album.name); 
+                console.log("\r\n------------------------------------\r\n")
+            }
+            
+            });
+    break;
+
+    case "movie-this":
+        axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
+            function(response) {
+                console.log("-------------- MOVIE GUIDE: " + userInput + " --------------\r\n") 
+                console.log(response.data.Title);
+                console.log("Released: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log(response.data.Ratings[1].Source + " Rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Starring: " + response.data.Actors); 
+                console.log("\r\n------------------------------------\r\n")
+                
+            })
+              
 
 }
 
